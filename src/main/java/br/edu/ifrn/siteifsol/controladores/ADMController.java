7f4f92c7f6a196e2ab172355a1854bfbcb1a00a3
oh-netin <severinocarvalho14@gmail.com>
2021-08-23 15:10:31 -0300

    Acesso a página de ADM

--- src/main/java/br/edu/ifrn/siteifsol/controladores/ADMController.java
+++ src/main/java/br/edu/ifrn/siteifsol/controladores/ADMController.java
@@ -2,15 +2,14 @@ package br.edu.ifrn.siteifsol.controladores;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ADMController {
-
-	@GetMapping("/admin")
-	public String home() {
+	
+	@PostMapping("/admin")
+	public String logado() {
 		return "ADM";
 	}
-	
-	
 
 }
