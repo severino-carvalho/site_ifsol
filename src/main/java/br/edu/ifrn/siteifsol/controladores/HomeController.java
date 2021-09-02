3bf1c02a26ad6e57826d51e1fa845e63db973803
oh-netin <severinocarvalho14@gmail.com>
2021-09-02 12:15:12 -0300

    Mostrar as notícias cadastradas na página inicial

--- src/main/java/br/edu/ifrn/siteifsol/controladores/HomeController.java
+++ src/main/java/br/edu/ifrn/siteifsol/controladores/HomeController.java
@@ -1,13 +1,28 @@
 package br.edu.ifrn.siteifsol.controladores;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.edu.ifrn.siteifsol.dominio.Noticia;
+import br.edu.ifrn.siteifsol.repository.NoticiaRepository;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	NoticiaRepository noticiaRepository;
+	
 	@GetMapping("/publico/home")
-	public String home() {
+	public String home(ModelMap modelo) {
+		List<Noticia> noticias = noticiaRepository.findAll();
+		
+		modelo.addAttribute("noticias", noticias);
+		
 		return "Home";
 	}
+	
 }
