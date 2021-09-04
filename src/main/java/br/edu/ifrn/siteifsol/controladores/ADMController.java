2909beb9edf7ac00ed11f1a1cb0f5d09b7b2a5a9
oh-netin <severinocarvalho14@gmail.com>
2021-09-04 18:46:09 -0300

    Atualização internas

--- src/main/java/br/edu/ifrn/siteifsol/controladores/ADMController.java
+++ src/main/java/br/edu/ifrn/siteifsol/controladores/ADMController.java
@@ -1,13 +1,43 @@
 package br.edu.ifrn.siteifsol.controladores;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.edu.ifrn.siteifsol.dominio.Noticia;
+import br.edu.ifrn.siteifsol.repository.NoticiaRepository;
 
 @Controller
 public class ADMController {
+	@Autowired
+	NoticiaRepository noticiaRepository;
 
 	@GetMapping("/adm")
 	public String home() {
 		return "ADM";
+	}
+
+	@GetMapping("/publico/noticia/{id}")
+	public String buscarNoticia(@PathVariable("id") Integer idNoticia, ModelMap modelo, RedirectAttributes attr) {
+
+		try {
+			Optional<Noticia> noticiaEncontrada = noticiaRepository.findById(idNoticia);
+
+			if (noticiaEncontrada.isPresent()) {
+				modelo.addAttribute("noticia", noticiaEncontrada.get()); // RETORNA A NOTICIA ENCONTRADA PARA A PÁGINA
+			} else {
+				attr.addFlashAttribute("msgNotErro", "");
+				return "redirect:/publico/home";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return "noticia/buscaNoticia";
 	}
 }
