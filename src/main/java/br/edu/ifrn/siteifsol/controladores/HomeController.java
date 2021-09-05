b361b14467412b63d4eb08f6c08eab4bdf7c3f15
oh-netin <severinocarvalho14@gmail.com>
2021-09-04 23:26:51 -0300

    Atualizações de melhorias

--- src/main/java/br/edu/ifrn/siteifsol/controladores/HomeController.java
+++ src/main/java/br/edu/ifrn/siteifsol/controladores/HomeController.java
@@ -1,11 +1,14 @@
 package br.edu.ifrn.siteifsol.controladores;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.edu.ifrn.siteifsol.dominio.Noticia;
 import br.edu.ifrn.siteifsol.repository.NoticiaRepository;
@@ -15,14 +18,33 @@ public class HomeController {
 
 	@Autowired
 	NoticiaRepository noticiaRepository;
-	
+
 	@GetMapping("/publico/home")
 	public String home(ModelMap modelo) {
 		List<Noticia> noticias = noticiaRepository.findAll();
-		
+
 		modelo.addAttribute("noticias", noticias);
-		
+
 		return "Home";
 	}
-	
+
+	@GetMapping("/publico/noticia/{id}")
+	public String buscarNoticia(@PathVariable("id") Integer idNoticia, ModelMap modelo, RedirectAttributes attr) {
+
+		try {
+			Optional<Noticia> noticiaEncontrada = noticiaRepository.findById(idNoticia);
+
+			if (noticiaEncontrada.isPresent()) {
+				modelo.addAttribute("noticia", noticiaEncontrada.get()); // RETORNA A NOTICIA ENCONTRADA PARA A PÁGINA
+			} else {
+				attr.addFlashAttribute("msgNotErro", "");
+				return "redirect:/publico/home";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return "noticia/buscaNoticia";
+	}
+
 }
--- src/main/java/br/edu/ifrn/siteifsol/dominio/Noticia.java
+++ src/main/java/br/edu/ifrn/siteifsol/dominio/Noticia.java
@@ -21,7 +21,7 @@ public class Noticia {
 	@Column(nullable = false)
 	private String titulo;
 
-	@Column(nullable = false)
+	@Column(nullable = false, length = 1000)
 	private String texto;
 
 	@Column(nullable = false)
